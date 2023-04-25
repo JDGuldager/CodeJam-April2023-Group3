9dd0d4836674ca13b89163fc60755a1257061d57
LeoLetsPlay3rd <113899208+LeoLetsPlay3rd@users.noreply.github.com>
2023-04-25 12:17:04 +0200

    SensorWorksNow

--- CodeJam/Assets/Scripts/SensorController.cs
+++ CodeJam/Assets/Scripts/SensorController.cs
@@ -6,6 +6,9 @@ public class SensorController : MonoBehaviour
 {
     Rigidbody2D rb;
     float dx;
+    float dy;
+
+    [SerializeField]
     float moveSpeed = 20f;
 
     // Start is called before the first frame update
@@ -19,10 +22,13 @@ public class SensorController : MonoBehaviour
     {
         dx = Input.acceleration.x * moveSpeed;
         transform.position = new Vector2(Mathf.Clamp(transform.position.x, -7.5f, 7.5f), transform.position.y);
+
+        dy = Input.acceleration.y * moveSpeed;
+        // transform.position = new Vector2(Mathf.Clamp(transform.position.y, -7.5f, 7.5f), transform.position.x);
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(dx, 0f);
+        rb.velocity = new Vector2(dx, dy);
     }
 }
