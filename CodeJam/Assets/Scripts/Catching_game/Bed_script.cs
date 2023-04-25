427bd999c4c9ae641791c9db460e1e732f3f5004
Simon <gulfurs@gmail.com>
2023-04-25 16:03:46 +0200

    catching game

---
+++ CodeJam/Assets/Scripts/Catching_game/Bed_script.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bed_script : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float dx;
+    float dy;
+
+    [SerializeField]
+    float moveSpeed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dx = Input.acceleration.x * moveSpeed;
+        //transform.position = new Vector2(Mathf.Clamp(transform.position.x, -7.5f, 7.5f), transform.position.y);
+
+        dy = Input.acceleration.y * moveSpeed;
+        // transform.position = new Vector2(Mathf.Clamp(transform.position.y, -7.5f, 7.5f), transform.position.x);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(dx, 0f);
+    }
+}
