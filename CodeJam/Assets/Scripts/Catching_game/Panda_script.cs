68f2e89b17dfd3a6badfd486cdb1c43fecdf09b0
Simon <gulfurs@gmail.com>
2023-04-26 16:37:23 +0200

    WORKING!!

--- CodeJam/Assets/Scripts/Catching_game/Panda_script.cs
+++ CodeJam/Assets/Scripts/Catching_game/Panda_script.cs
@@ -1,43 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Panda_script : MonoBehaviour
 {
     
     public GameObject PandaPreFab;
-    public int numPanda = 10;
+   
+    public Stork_script storkScript;
+    //[SerializeField] private TextMeshProUGUI tmp;
+
+    public int numPanda = 1;
     public int minX = -2;
     public int maxX = 2;
     public int y = 0;
-
     bool IsPandaSpawned = false;
+
+  
 
     void Start()
     {
-
+        Debug.Log("Restart");
+        //score = 0;
          
     }
 
     void FixedUpdate()
     {
+        //tmp.text = score.ToString();
     }
+    //The idea for making a bool to check the prefab, and calling the class on collision is from this video https://www.youtube.com/watch?v=IXDvl8aTM_M
+    
     private void SpawnPanda(){
-        //  for (int i = 0; i < numPanda; i++){
-            
-        //     Instantiate(PandaPreFab, new Vector3(x, y, 0f), Quaternion.identity);
-        // } 
-         if (IsPandaSpawned == false){
+         if (IsPandaSpawned == false || numPanda < 10) { 
+            numPanda ++;
             float x = Random.Range(minX, maxX);
-            Vector3 PanPosition = new Vector3(0, 4f, 0f); //Makes the panda spawn on a random range on x axis 
-            Instantiate(PandaPreFab, PanPosition, Quaternion.identity); //Instantiate the prefab at the random loc
+            Vector3 PanPosition = new Vector3(0, 4f, 0f); //Makes the panda spawn on a random range on x axis
+            Instantiate(PandaPreFab, PanPosition, Quaternion.identity); //Instantiate the prefab at the random loc (This line have been gotten through ChatBot)
             IsPandaSpawned = true;
+            //Debug.Log(score);
+           // 
         }
+        
     }
      private void OnTriggerEnter2D (Collider2D collision){
-        
-        SpawnPanda(); //Spawn new when collided
+        //storkScript.SpawnPanda(); //Spawn new when collided
+        //storkScript.SpawnPanda();
+        SpawnPanda();
+        storkScript.score++;
         Destroy(PandaPreFab);
+        
         //IsPandaSpawned = false;
     }
 }
