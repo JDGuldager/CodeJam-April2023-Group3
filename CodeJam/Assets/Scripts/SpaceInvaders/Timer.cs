786eabd3c007251fdaaafcbf18e2f94d7e66bf08
FelixIuel <113988860+FelixIuel@users.noreply.github.com>
2023-04-26 18:59:25 +0200

    Timer kan nu bruges til flere spil
    Der kan bruges både til at tabe eller til at vinde

--- CodeJam/Assets/Scripts/SpaceInvaders/Timer.cs
+++ CodeJam/Assets/Scripts/SpaceInvaders/Timer.cs
@@ -8,32 +8,48 @@ using UnityEngine.UI;
 
 //kilder:
 //https://gamedevbeginner.com/how-to-make-countdown-timer-in-unity-minutes-seconds/
-//Chatgbt
 public class Timer : MonoBehaviour
 {
     public Text timeLeft;
     public bool timerIsRunning = false;
-
-    [SerializeField]
     public float timeTillWin = 30f;
+    public float timeTillLose = 30f;
+    public bool WinTimerBool = false;
+    public bool LoseTimerBool = false;
 
     private void Start()
     {
         // Starts the timer automatically
         timerIsRunning = true;
+
     }
     void Update()
     {
-        if (timeTillWin > 0)
+        if (WinTimerBool == true)
         {
-            timeTillWin -= Time.deltaTime;
-        }
-        else
-        {
-            Invoke("WinGame", 1f);
+            DisplayWinTime();
+            if (timeTillWin > 0)
+            {
+                timeTillWin -= Time.deltaTime;
+            }
+            else
+            {
+                Invoke("WinGame", 1f);
+            }
         }
 
-        DisplayTime();
+        if (LoseTimerBool == true)
+        {
+            DisplayLoseTime();
+            if (timeTillLose > 0)
+            {
+                timeTillLose -= Time.deltaTime;
+            }
+            else
+            {
+                Invoke("LoseGame", 1f);
+            }
+        }
     } 
 
 public void WinGame(int SceneNum)
@@ -41,10 +57,21 @@ public void WinGame(int SceneNum)
     SceneManager.LoadScene(0);
 }
 
-void DisplayTime()
+public void LoseGame(int SceneNum)
+    {
+        SceneManager.LoadScene(0);
+    }
+void DisplayWinTime()
 {
 float minutes = Mathf.FloorToInt(timeTillWin / 60);
 float seconds = Mathf.FloorToInt(timeTillWin % 60);
+timeLeft.text = string.Format("TIME LEFT: {0:00}:{1:00}", minutes, seconds);
+}
+
+void DisplayLoseTime()
+{
+float minutes = Mathf.FloorToInt(timeTillLose / 60);
+float seconds = Mathf.FloorToInt(timeTillLose % 60);
 timeLeft.text = string.Format("TIME LEFT: {0:00}:{1:00}", minutes, seconds);
 }
 }
