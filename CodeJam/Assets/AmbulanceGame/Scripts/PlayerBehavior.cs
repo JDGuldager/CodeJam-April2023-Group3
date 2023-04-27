c0a4f3b8ce3aca13a011253e460bd46f0c021602
laurman1 <laur8250@hotmail.dk>
2023-04-27 12:28:45 +0200

    asd

---
+++ CodeJam/Assets/AmbulanceGame/Scripts/PlayerBehavior.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*public class PlayerBehavior : MonoBehaviour
+{
+    private RigidbodyConstraints rbc;
+    
+
+    private void Start()
+    {
+        rbc = GetComponent<RigidbodyConstraints>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            rbc = RigidbodyConstraints.FreezePosition;
+        }
+    }
+}
+*/
