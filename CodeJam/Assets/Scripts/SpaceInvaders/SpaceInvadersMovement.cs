3d6c007e87d1312210fb43e300ff1851703b4237
FelixIuel <113988860+FelixIuel@users.noreply.github.com>
2023-04-27 10:18:58 +0200

    Space Invaders test 2
    The Aliens are comming

---
+++ CodeJam/Assets/Scripts/SpaceInvaders/SpaceInvadersMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Kilder:
+//StackItSensors
+public class SpaceInvadersMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float dx;
+    float dy;
+    private float min_x = -1.55f, max_x = 1.55f;
+    [SerializeField]
+    float moveSpeed = 20f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dx = Input.acceleration.x * moveSpeed;
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, min_x, max_x), transform.position.y);
+
+        dy = Input.acceleration.y * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(dx, dy);
+    }
+}
