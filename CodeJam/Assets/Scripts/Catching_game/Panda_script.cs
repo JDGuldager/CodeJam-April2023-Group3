45eecbcb0f7e2d13987ab690604da9212c8e8b3b
fijibottle <gulfurs@gmail.com>
2023-05-05 10:45:11 +0200

    yesssssss

--- CodeJam/Assets/Scripts/Catching_game/Panda_script.cs
+++ CodeJam/Assets/Scripts/Catching_game/Panda_script.cs
@@ -7,55 +7,46 @@ using UnityEngine.SceneManagement;
 
 public class Panda_script : MonoBehaviour
 {
-    
     public GameObject PandaPreFab;
     public GameObject spawnPos;
     public Stork_script storkScript;
     [SerializeField] private TextMeshProUGUI tmp;
-    //private int minX = -2;
-   // private int maxX = 2;
-    public int y = 0;
     bool IsPandaSpawned = false;
 
     public AudioClip soundsound;
+    public int score = 0;
 
-    void Start()
-    {
-        Debug.Log("Restart");
-        //score = 0;
-         
+
+    void Start(){
+        Debug.Log(score);
     }
 
-    void FixedUpdate()
-    {
-      
-    }
-    //The idea for making a bool to check the prefab, and calling the class on collision is from this video https://www.youtube.com/watch?v=IXDvl8aTM_M
-    
+    //The idea for making a bool to check the prefab, and calling the class on collision is from this video https://www.youtube.com/watch?v=IXDvl8aTM_M    
     private void SpawnPanda(){
-         if (IsPandaSpawned == false || spawnPos.transform.position.y < 3.9f) { 
-            //float x = Random.Range(minX, maxX); //The range the 
-            //Vector3 PanPosition = new Vector3(2.5f, 4f, 0f); 
-            Instantiate(PandaPreFab, spawnPos.transform.position, Quaternion.identity); //Instantiate the prefab at the random loc (This line was originally from ChatBot, but changed since)
+        score++;
+         if (IsPandaSpawned == false) { //Only spawn when there isnt a panda on screen
+            Instantiate(PandaPreFab, spawnPos.transform.position, Quaternion.identity); //Instantiate the prefab at the random loc (This idea was originally from ChatBot, but changed since)
             IsPandaSpawned = true;
- 
         }
         
     }
      private void OnTriggerEnter2D (Collider2D target){
         SpawnPanda(); //Spawn new when collided
         Destroy(PandaPreFab); //Destroy the Prefab 
-        if(target.gameObject.tag == "Bed")
+        if(target.gameObject.tag == "Bed") //collison with the bed
         {
-            Debug.Log("Registered" + storkScript.score);
-            storkScript.score++; //The score goes up by one, each time they collide
-            tmp.text = storkScript.score.ToString(); //And update the score on screen
-            SoundManager.instance.PlaySound(soundsound);
+            
+            //storkScript.score++; //The score goes up by one, each time they collide
+            //tmp.text = storkScript.score.ToString(); //And update the score on screen
+            tmp.text = score.ToString(); //And update the score on screen
+            SoundManager.instance.PlaySound(soundsound); //And plays a sound effect
         } else {
-           Timer.GameOver();
+           Timer.GameOver(); 
+           //If you dont collide with the Bed tag, the collision happens with a cathing
+           //That loses you the game
         }
-        if(storkScript.score >= 10) {
-            Timer.NextScene(); //If the score is 10, change scene
+        if(score >= 10) {
+            Timer.NextScene(); //If the score is 10, change scene, from the timer script
         }
         
     }
